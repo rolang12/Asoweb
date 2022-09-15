3e438c035a90728f71033ae0c5e65e737389a3f0
rolang12 <rolandpm09@gmail.com>
2022-09-15 10:19:16 -0500

    estilos de mensaje de notificacion v2

--- resources/views/livewire/notificacion-mensaje.blade.php
+++ resources/views/livewire/notificacion-mensaje.blade.php
@@ -1,17 +1,29 @@
 {{-- {{dd($notificaciones)}} --}}
 <div class="my-auto" >
     <div x-data="{ open: false }">
-        <i  x-on:click="open=!open" class="fa-solid fa-message text-white hover:text-gray-200 text-lg px-4 "></i>
-
+        
+        @if( count($notificaciones) > 0 )
+            <div>
+                <i  x-on:click="open=!open" class="fa-solid fa-message text-white hover:text-gray-200 text-lg px-4 "></i>
+                <i class="-ml-6  text-red-500 fa-solid fa-circle"></i>
+                <span class="text-white" >{{count($notificaciones)}}</span>
+            </div>
+        @else
+            <i  x-on:click="open=!open" class="fa-solid fa-message text-white hover:text-gray-200 text-lg px-4 "></i>
+        @endif
+        
         <div x-show="open" x-on:click.away="open = false" class="bg-gray-50 my-3 absolute">
 
             <div class="">
 
                 <ul class="bg-gray-50 " >
                 @forelse ($notificaciones as $notificacion)
-                    <li class="px-4 py-2   bg-gray-50 shadow-lg w-44 right-36 hover:bg-blue-100 text-left ">
-                        <div class="text-sm" >{{$notificacion->user->name}} Te ha enviado un mensaje</div> 
-                        
+                    <li class="px-4 py-2 bg-gray-50 shadow-lg w-44 right-36 hover:bg-gray-100 text-left ">
+
+                        <a href="{{route('chatify/',['id' => $notificacion->from_id])}}" >
+
+                        <div class="text-xs font-bold text-blue-800" >{{$notificacion->user->name}}</div><div class="text-xs"> Te ha enviado un mensaje</div> 
+                        </a>
                         {{-- <a href="{{ route('perfil', ['id' => $detalle->users->name]) }}"
                             class="text-right text-sm text-blue-800">{{ $detalle->users->name }}</a> --}}
                     </li>
