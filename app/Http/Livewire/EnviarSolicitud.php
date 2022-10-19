46aef9fb6889792e26e7571067fc249f8068adf1
rolang12 <rolandpm09@gmail.com>
2022-10-19 11:37:07 -0500

    add trait UserServices

--- app/Http/Livewire/EnviarSolicitud.php
+++ app/Http/Livewire/EnviarSolicitud.php
@@ -4,11 +4,14 @@ namespace App\Http\Livewire;
 
 use App\Models\Amigos;
 use App\Models\Usuarios_has_amigos;
+use App\Services\UserServices;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class EnviarSolicitud extends Component
 {
+    use UserServices;
+
     public $iduser = '', $status, $sonAmigos;
 
     public function mount()
@@ -24,34 +27,37 @@ class EnviarSolicitud extends Component
         //Verificar si tengo una solicitud recibida
         $sonAmigos2 = Amigos::where('from_id',$this->iduser)->where('to_id', Auth::user()->id )->get();
 
+        // Si no tengo una solicitud de el usuario, genero la plantilla para enviar una solicitud
         if ($sonAmigos2->isEmpty()) {
             
+            // Si aun no tengo solicitudes hacia ese usuario, la inicializo
             if ($sonAmigos->isEmpty()) {
                 $this->status = 'Enviar Solicitud';
 
             } else {
+                // Si he enviado una solicitud hacia ese usuario, obtengo el status
                 $this->status = $sonAmigos[0]->status;
                 $sonAmigos[0]->status;
                 
             }
-                
+            
+            // Retorno la vista con el status actual
             return view('livewire.enviar-solicitud',[
                 'status' => $this->status
             ]);
            
         } else {
-
+            //Si tengo una solicitud de el usuario, verifico si ya son amigos o no
             if ($sonAmigos2[0]->status == 'Solicitud Enviada') {
                 $this->status =  'Aceptar Solicitud';
+
             } elseif ($sonAmigos2[0]->status == 'Amigos') {
                 $this->status =  'Amigos';
 
             }
 
             return view('livewire.enviar-solicitud',[
-
                 'status' => $this->status
-
             ]);
         }
 
@@ -97,14 +103,9 @@ class EnviarSolicitud extends Component
         //Si los usuarios son Amigos, los elimino en ambas direcciones
         if ($sonAmigos[0]->status == 'Amigos') {
 
-            $usuarioHasAmigo = Usuarios_has_amigos::where('users_id', Auth::user()->id)
-                                ->where('friends_id', $iduser);
-                                
-            $amigoHasUsuario = Usuarios_has_amigos::where('users_id', $iduser )
-                                ->where('friends_id', Auth::user()->id );
-
-            $usuarioHasAmigo->delete();
-            $amigoHasUsuario->delete();
+            // Llamo al trait para eliminar usuarios amigos
+            UserServices::deleteFriends($iduser, Auth::user()->id);
+            UserServices::deleteFriends(Auth::user()->id, $iduser);
 
             $sonAmigos[0]->update([
                 'status' => 'Enviar Solicitud',
@@ -118,8 +119,6 @@ class EnviarSolicitud extends Component
     public function aceptarSolicitud()
     {
         $sonAmigos2 = Amigos::where('from_id',$this->iduser)->where('to_id', Auth::user()->id )->get();
-
-        // $this->status = 'Solicitud Enviada';
 
         $sonAmigos2[0]->update([
             'status' => 'Amigos',
--- app/Services/UserServices.php
+++ app/Services/UserServices.php
@@ -2,19 +2,26 @@
 
 namespace App\Services;
 
-use App\Http\Controllers\Controller;
 use App\Models\Publicaciones;
 use App\Models\Usuarios_has_amigos;
 
-class UserServices extends Controller {
+trait UserServices {
 
-    public static function getFriends($id) {
-      return $friendsCount = Usuarios_has_amigos::where('users_id',$id )->get('friends_id')->count();
+  public function getFriends($id) {
+    return $friendsCount = Usuarios_has_amigos::where('users_id',$id )->get('friends_id')->count();
 
-    }
-    public static function getPostCount($id) {
-      return $postCount = Publicaciones::where('users_id',$id )->get('id')->count();
-    }
+  }
+  public function getPostCount($id) {
+    return $postCount = Publicaciones::where('users_id',$id )->get('id')->count();
+  }
 
+  public function deleteFriends($id, $auth) {
+
+    $usuarioHasAmigo = Usuarios_has_amigos::where('users_id', $auth)
+    ->where('friends_id', $id);
+
+    return $usuarioHasAmigo->delete();
+
+  }
 
 }
