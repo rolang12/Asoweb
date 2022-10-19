11437b8e3793c3e104c13a8594789cb1bc4948cc
rolang12 <rolandpm09@gmail.com>
2022-10-19 09:38:31 -0500

    configurando perfil enviar solicitudes v4

--- app/Http/Livewire/EnviarSolicitud.php
+++ app/Http/Livewire/EnviarSolicitud.php
@@ -14,6 +14,7 @@ class EnviarSolicitud extends Component
 
     public function mount()
     {
+        $this->status = "Enviar Solicitud";
         // dd($this->sonAmigos->status);
     }
 
@@ -22,7 +23,7 @@ class EnviarSolicitud extends Component
         $sonAmigos = Amigos::where('from_id',Auth::user()->id)->where('to_id', $this->iduser)->get();
 
         if ($sonAmigos->isEmpty()) {
-             $this->status = 'Enviar Solicitud';
+            $this->status = 'Enviar Solicitud';
         } else {
             $this->status = $sonAmigos[0]->status;
             $sonAmigos[0]->status;
@@ -37,9 +38,10 @@ class EnviarSolicitud extends Component
 
     public function enviarSolicitud($iduser)
     {
-        
-        if (isEmpty($this->sonAmigos)) {
-            $this->sonAmigos = Amigos::create([
+        $sonAmigos = Amigos::where('from_id',Auth::user()->id)->where('to_id', $iduser)->limit(1)->get();
+
+        if ($sonAmigos->isEmpty()) {
+            $sonAmigos = Amigos::create([
                 'from_id' => Auth::user()->id,
                 'to_id' => $this->iduser,
                 'status' => 'Solicitud Enviada',
@@ -48,14 +50,25 @@ class EnviarSolicitud extends Component
             return $this->status = 'Solicitud Enviada';
         }
 
-        if ($this->sonAmigos->status == 'Solicitud Enviada') {
-        
-            $this->sonAmigos = Amigos::update([
-                'status' => 'Cancelar Solicitud';
+        if ($sonAmigos[0]->status == 'Solicitud Enviada') {
+            $this->status = 'Enviar Solicitud';
+
+            $sonAmigos[0]->update([
+                'status' => 'Enviar Solicitud',
             ]);
 
-            return $this->status = 'Cancelar Enviada';
-
+            return $this->status = 'Enviar Solicitud';
         }
+
+        // if ($sonAmigos[0]->status == 'Solicitud Cancelada') {
+        
+        //     $sonAmigos[0]->update([
+        //         'status' => 'Enviar Solicitud',
+        //     ]);
+
+        //     return $this->status = 'Enviar Solicitud';
+        // }
+
+
     }
 }
